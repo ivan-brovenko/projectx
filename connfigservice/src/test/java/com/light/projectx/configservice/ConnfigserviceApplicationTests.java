2b3c60a564b34dbf881678c3534be4aa107a1ccc
Ivan_Brovenko <ivan.brovenko@epam.com>
2020-05-10 19:11:49 +0300

    configservice:init

---
+++ connfigservice/src/test/java/com/light/projectx/configservice/ConnfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.light.projectx.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnfigserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
